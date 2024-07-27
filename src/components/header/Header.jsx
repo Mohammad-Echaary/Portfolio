a62fd3bf949ea35df346bce0ac0b803980c4791c
Mohammad Echaary <mohammadechaary@gmail.com>
2024-07-27 20:46:49 +0100

    added the components and reset css

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,4 +1,19 @@
 import "./header.css";
 export default function Header() {
-  return <h1>Header</h1>;
+  return (
+    <header>
+      <section className="container">
+        <h5>hello, i am</h5>
+        <h1>mohammad echaary</h1>
+        <h5>a front end developer</h5>
+        <div className="buttons">
+          <button className="btn">download CV</button>
+          <button className="btn btn-primary">lets talk</button>
+        </div>
+        <div className="picture">
+          <img src="../../assets/me.png" alt="" />
+        </div>
+      </section>
+    </header>
+  );
 }
